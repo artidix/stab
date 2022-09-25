c7b78b1f65d1c0b87973271205ce23a52cc25a9b
Albert Garuda <algarud@gmail.com>
2022-09-25 12:57:54 +0300

    use api key

--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
 import { generate } from 'stability-ts'
+require('dotenv').config();
 
 const api = generate({
   prompt: 'A Stunning House',
-  apiKey: '',
-  width: 512,
-  height: 512,
-  outDir: 'img',
-  steps: 50,
-  cfgScale: 7,
-  engine: 'stable-diffusion-v1',
-  diffusion: "k_lms",
-  seed: 3465383516,
-  debug: false,
-  samples: 1,
-  requestId: "34243242",
-  noStore: true,
+  apiKey: process.env.STABILITY_API_KEY,
+  // width: 512,
+  // height: 512,
+  // outDir: 'img',
+  // steps: 50,
+  // cfgScale: 7,
+  // engine: 'stable-diffusion-v1',
+  // diffusion: "k_lms",
+  // seed: 3465383516,
+  // debug: false,
+  // samples: 1,
+  // requestId: "34243242",
+  // noStore: true,
   // host: 'grpc.stability.ai:443'
 })
 
